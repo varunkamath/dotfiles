62c4f689635141ee0718daf86f6543820f81632d
Varun Kamath <varun.kamath@gmail.com>
2025-05-21 23:10:02 -0500

    first commit

---
+++ config.fish
@@ -0,0 +1,29 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function starship_transient_rprompt_func
+  starship module time
+end
+starship init fish | source
+enable_transience
+
+zoxide init fish | source
+
+set DISABLE_UNTRACKED_FILES_DIRTY "true"
+
+export PATH="/opt/homebrew/Cellar/tcl-tk/bin:$PATH"
+
+if test -f "$HOME/.cargo/env"
+    fish_add_path "$HOME/.cargo/bin"
+end
+
+if test -f "$HOME/.local/bin/env"
+    fish_add_path "$HOME/.local/bin"
+end
+
+alias ls "lsd"
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
