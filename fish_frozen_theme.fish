53953d7dc28a150b0af7ec80d1f5390ae0af89e0
Varun Kamath (US) <varun.kamath@lmco.com>
2026-02-17 11:25:51 -0500

    Add fish theme

---
+++ fish_frozen_theme.fish
@@ -0,0 +1,38 @@
+set --global fish_color_autosuggestion brblack
+set --global fish_color_cancel --reverse
+set --global fish_color_command green
+set --global fish_color_comment yellow
+set --global fish_color_cwd green
+set --global fish_color_cwd_root red
+set --global fish_color_end brmagenta
+set --global fish_color_error red
+set --global fish_color_escape brcyan
+set --global fish_color_history_current --bold
+set --global fish_color_host normal
+set --global fish_color_host_remote yellow
+set --global fish_color_keyword green
+set --global fish_color_match cyan
+set --global fish_color_normal normal
+set --global fish_color_operator cyan
+set --global fish_color_option normal
+set --global fish_color_param normal
+set --global fish_color_quote yellow
+set --global fish_color_redirection brblue
+set --global fish_color_search_match bryellow --background=brblack
+set --global fish_color_selection white --bold --background=brblack
+set --global fish_color_status red
+set --global fish_color_user brgreen
+set --global fish_color_valid_path --underline
+set --global fish_pager_color_background
+set --global fish_pager_color_completion normal
+set --global fish_pager_color_description yellow
+set --global fish_pager_color_prefix normal --bold --underline
+set --global fish_pager_color_progress brwhite --background=cyan
+set --global fish_pager_color_secondary_background
+set --global fish_pager_color_secondary_completion
+set --global fish_pager_color_secondary_description
+set --global fish_pager_color_secondary_prefix
+set --global fish_pager_color_selected_background --background=brblack
+set --global fish_pager_color_selected_completion
+set --global fish_pager_color_selected_description
+set --global fish_pager_color_selected_prefix
